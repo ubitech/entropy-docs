133e960c3235ec70deb6e06942431c914e07ee00
Giannis Ledakis <g.ledakis@gmail.com>
2017-11-06 11:10:34 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Mitigate Documentation'
-copyright = u'2017, Mitigate consortium'
+project = u'Entropy Documentation'
+copyright = u'2017, Entropy consortium'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
